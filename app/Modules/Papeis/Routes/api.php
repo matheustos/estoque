c0b178a8bd7cdb0f1d848395bf575db3f6b052e0
matheustos <127261813+matheustos@users.noreply.github.com>
2025-11-27 14:57:09 -0300

    Descomentando permissões

--- app/Modules/Papeis/Routes/api.php
+++ app/Modules/Papeis/Routes/api.php
@@ -7,10 +7,10 @@ use App\Modules\Papeis\Controllers\PapeisPermissoesController;
 
 // Rotas protegidas por autenticação
 Route::middleware('auth:api')->group(function () {
-    Route::post('/papeis', [PapeisController::class, 'store']);
-    //->middleware('acesso:criar_papeis');
-    Route::post('/permissoes', [PapeisPermissoesController::class, 'atribuirPermissoes']);
-   // ->middleware('acesso:criar_permissoes');
-    Route::get('/permissoes', [PermissoesController::class, 'index']);
-    //->middleware('acesso:ver_permissoes');
+    Route::post('/papeis', [PapeisController::class, 'store'])
+    ->middleware('acesso:criar_papeis');
+    Route::post('/permissoes', [PapeisPermissoesController::class, 'atribuirPermissoes'])
+    ->middleware('acesso:criar_permissoes');
+    Route::get('/permissoes', [PermissoesController::class, 'index'])
+    ->middleware('acesso:ver_permissoes');
 });
