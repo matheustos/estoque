1cc8c7b5db864559609ca6d87a26aa21894260a7
matheustos <127261813+matheustos@users.noreply.github.com>
2025-11-28 13:05:55 -0300

    novas permissões criadas

--- app/Modules/Almoxarifados/Routes/api.php
+++ app/Modules/Almoxarifados/Routes/api.php
@@ -5,13 +5,14 @@ use App\Modules\Almoxarifados\Controllers\AlmoxarifadosController;
 
 // Rotas protegidas por autenticação
 Route::middleware('auth:api')->group(function () {
-    Route::get('/almoxarifados/{id}',  [AlmoxarifadosController::class, 'show']);
-});
-
-// Rotas protegidas apenas para admins
-Route::middleware('auth:api')->group(function(){
-    Route::post('/almoxarifados', [AlmoxarifadosController::class, 'store']);
-    Route::get('/almoxarifados', [AlmoxarifadosController::class, 'index']);
-    Route::put('/almoxarifados/{id}', [AlmoxarifadosController::class, 'update']);
-    Route::delete('/almoxarifados/{id}', [AlmoxarifadosController::class, 'destroy']);
+    Route::get('/almoxarifados/{id}',  [AlmoxarifadosController::class, 'show'])
+    ->middleware('acesso: ver_almoxarifado_id');
+    Route::post('/almoxarifados', [AlmoxarifadosController::class, 'store'])
+    ->middleware('acesso: criar_almoxarifado');
+    Route::get('/almoxarifados', [AlmoxarifadosController::class, 'index'])
+    ->middleware('acesso: ver_almoxarifados');
+    Route::put('/almoxarifados/{id}', [AlmoxarifadosController::class, 'update'])
+    ->middleware('acesso: atualizar_almoxarifado');
+    Route::delete('/almoxarifados/{id}', [AlmoxarifadosController::class, 'destroy'])
+    ->middleware('acesso: deletar_almoxarifado');
 });
--- app/Modules/Categorias/Routes/api.php
+++ app/Modules/Categorias/Routes/api.php
@@ -5,9 +5,14 @@ use App\Modules\Categorias\Controllers\CategoriasController;
 
 // Rotas protegidas por autenticação
 Route::middleware('auth:api')->group(function () {
-    Route::post('/categorias',  [CategoriasController::class, 'store']);
-    Route::get('/categorias',  [CategoriasController::class, 'index']);
-    Route::get('/categorias/{id}',  [CategoriasController::class, 'show']);
-    Route::put('/categorias/{id}', [CategoriasController::class, 'update']);
-    Route::delete('/categorias/{id}', [CategoriasController::class, 'destroy']);
+    Route::post('/categorias',  [CategoriasController::class, 'store'])
+    ->middleware('acesso: criar_categoria');
+    Route::get('/categorias',  [CategoriasController::class, 'index'])
+    ->middleware('acesso: ver_categorias');
+    Route::get('/categorias/{id}',  [CategoriasController::class, 'show'])
+    ->middleware('acesso: ver_categoria_id');
+    Route::put('/categorias/{id}', [CategoriasController::class, 'update'])
+    ->middleware('acesso: atualizar_categoria');
+    Route::delete('/categorias/{id}', [CategoriasController::class, 'destroy'])
+    ->middleware('acesso: deletar_categoria');
 });
